3325f4120fffb84a0cc09adc5a2a8bb6ee4c9402
Markus Holopainen <markus.x.holopainen@helsinki.fi>
2022-05-20 15:47:10 +0300

    e1.8+e1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,12 +4,27 @@ const Header = ({title}) => <h1>
   {title}
 </h1>
 
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {all > 0 ? (good - bad) / all : 0}</p>
+      <p>positive { all > 0 ? (good / all) * 100 : 0} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + neutral + bad
   return (
     <div>
       <Header title={'give feedback'}/>
@@ -17,9 +32,8 @@ const App = () => {
       <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
       <button onClick={()=>setBad(bad+1)}>bad</button>
       <Header title={'statistics'}/>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+
     </div>
   )
 }
