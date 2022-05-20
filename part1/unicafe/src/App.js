9b07d67df02690937ce379146bca015efab93cb5
Markus Holopainen <markus.x.holopainen@helsinki.fi>
2022-05-20 16:06:32 +0300

    e1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,20 +4,25 @@ const Header = ({title}) => <h1>
   {title}
 </h1>
 
-const StatisticLine = ({text, value}) => <p>{text} {value}</p>
+const StatisticLine = ({text, value, percentage}) => <tr>
+    <td>{text}</td>
+    <td>{value} {percentage ? '%' : ''}</td>
+  </tr>
 
 const Statistics = ({good, neutral, bad}) => {
   const all = good + neutral + bad
   
   return all > 0 ?  (
-    <div>
-      <StatisticLine text={'good'} value={good}/>
-      <StatisticLine text={'neutral'} value={neutral}/>
-      <StatisticLine text={'bad'} value={bad}/>
-      <StatisticLine text={'all'} value={all}/>
-      <StatisticLine text={'average'} value={all > 0 ? (good - bad) / all : 0}/>
-      <StatisticLine text={'positive'} value={all > 0 ? (good / all) * 100 : 0}/>
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text={'good'} value={good}/>
+        <StatisticLine text={'neutral'} value={neutral}/>
+        <StatisticLine text={'bad'} value={bad}/>
+        <StatisticLine text={'all'} value={all}/>
+        <StatisticLine text={'average'} value={all > 0 ? (good - bad) / all : 0}/>
+        <StatisticLine text={'positive'} value={all > 0 ? (good / all) * 100 : 0} percentage/>
+      </tbody>
+    </table>
   ) : (
     <p>No feedback given</p>
   )
