be3c93b375a605f7273d3aa8e13105b28feb2247
Holopainen <hgmarkus@lm7-cs-7.local>
2022-05-20 12:40:13 +0300

    1.2

--- part1/kurssitiedot/src/App.js
+++ part1/kurssitiedot/src/App.js
@@ -1,18 +1,16 @@
 const Header = ({course}) => <h1>{course}</h1>
 
+const Part = ({part, exercise}) => <p>
+    {part} {exercise}
+  </p>
+
 const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
   return (
-    <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-    </>
+    <div>
+      <Part part={part1} exercise={exercises1}/>
+      <Part part={part2} exercise={exercises2}/>
+      <Part part={part3} exercise={exercises3}/>
+    </div>
   )
 }
 
