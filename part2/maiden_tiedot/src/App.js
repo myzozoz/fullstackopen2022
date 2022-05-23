592c5e9549396c57ee2e23371ed44a8df3d85681
Markus Holopainen <markus.x.holopainen@helsinki.fi>
2022-05-23 16:36:54 +0300

    e2.13

--- part2/maiden_tiedot/src/App.js
+++ part2/maiden_tiedot/src/App.js
@@ -8,26 +8,36 @@ const Filter = ({value, changeHandler}) =>
       onChange={changeHandler}/>
   </div>
 
-const Country = ({country, detailed}) =>
+const Country = ({country, detailed, showCallback}) =>
   detailed ? 
-    <div>
-      <h1>{country.name.common}</h1>
-      <p>Capital: {country.capital[0]}</p>
-      <p>Area: {country.area}</p>
-      <h3>Languages:</h3>
-      <ul>
-        {Object.keys(country.languages).map(l => <li key={l}>{country.languages[l]}</li>)}
-      </ul>
-      <img src={country.flags.png} alt='flag'/>
-    </div>
-    : <p>{country.name.common}</p>
+    <CountryDetails country={country}/>
+    : <p>
+        {country.name.common}
+        <button onClick={() => showCallback(country.name.common)}>show</button>
+      </p>
 
+const CountryDetails = ({country}) => 
+  <div>
+    <h1>{country.name.common}</h1>
+    <p>Capital: {country.capital[0]}</p>
+    <p>Area: {country.area}</p>
+    <h3>Languages:</h3>
+    <ul>
+      {Object.keys(country.languages).map(l => <li key={l}>{country.languages[l]}</li>)}
+    </ul>
+    <img src={country.flags.png} alt='flag'/>
+  </div>
 
-const CountryList = ({countries}) => 
+const CountryList = ({countries, showCallback}) => 
   <div>
     {countries.length > 10
       ? <p>Too many matches, specify another filter</p> 
-      : countries.map(country => <Country key={country.name.common} country={country} detailed={countries.length === 1}/>)}
+      : countries.map(country =>
+        <Country
+          key={country.name.common}
+          country={country}
+          detailed={countries.length === 1}
+          showCallback={showCallback}/>)}
   </div>
 
 const App = () => {
@@ -43,11 +53,19 @@ const App = () => {
       })
   }, [])
 
+  const showHandler = (countryName) => {
+    console.log(countryName)
+    setFilterText(countryName)
+  }
+
+
   return (
     <div>
       <h1>Browse countries:</h1>
       <Filter value={filterText} changeHandler={(e) => setFilterText(e.target.value)}/>
-      <CountryList countries={countries.filter(c => c.name.common.toLowerCase().includes(filterText))}/>
+      <CountryList
+        countries={countries.filter(c => c.name.common.toLowerCase().includes(filterText.toLowerCase()))}
+        showCallback={showHandler}/>
     </div>
   )
 }
