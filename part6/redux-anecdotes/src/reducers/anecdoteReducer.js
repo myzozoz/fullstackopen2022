f284fb61f873b517fbbce855b1bd6c4e2785db12
Markus Holopainen <markusp_holopainen@yahoo.com>
2022-11-02 13:27:32 +0200

    6.3

---
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -0,0 +1,45 @@
+const anecdotesAtStart = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+]
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const asObject = (anecdote) => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0,
+  }
+}
+
+const initialState = anecdotesAtStart.map(asObject)
+
+const reducer = (state = initialState, action) => {
+  console.log('state now: ', state)
+  console.log('action', action)
+  switch (action.type) {
+    case 'VOTE':
+      const i = state.findIndex((a) => a.id === action.data.id)
+      if (i < 0) return state
+      const newAnecdote = { ...state[i], votes: state[i].votes + 1 }
+      const newState = [...state]
+      newState[i] = newAnecdote
+      return newState
+    default:
+      return state
+  }
+}
+
+export const voteFor = (id) => {
+  return {
+    type: 'VOTE',
+    data: { id },
+  }
+}
+
+export default reducer
